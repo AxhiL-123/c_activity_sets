247b2c9bf967fafbe45fa221a9f54893689e889a
Akhil <polisettys.bsc23@rvu.edu.in>
2024-02-25 23:17:53 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int num1,num2,sum;
-    Printf("Enter first number: ");
-    Scanf("%d",&num1);
-    Printf("Enter Second number: ");
-    Scanf("%d",&num2); 
-    Printf("Sum of %d and %d is %d\n",num1,num2,sum);
-    
+        int num1, num2, sum;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum of %d and %d is %d\n", num1, num2, sum);
+
     return 0;
-
 }
-
